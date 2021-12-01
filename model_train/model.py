def0e73cd81fb3392b09423b9fab2f4224697630
Matthew Huff <mwhuff@pop-os.localdomain>
2021-11-30 20:06:54 -0700

    update

--- model_train/model.py
+++ model_train/model.py
@@ -96,6 +96,7 @@ class JointQAModel(BertPreTrainedModel):
         self.model = AutoModel.from_config(config)
         self.para_classifier = nn.Linear(config.hidden_size, config.num_labels)
         self.sent_classifier = ClassificationSent(config)
+        self.qa_output = nn.Linear(config.hidden_size, config.num_labels)
         self.use_sent_loss = config.use_sent_loss
         
         print("use sentence loss in the training: ", self.use_sent_loss)
@@ -137,10 +138,17 @@ class JointQAModel(BertPreTrainedModel):
             inputs_embeds=inputs_embeds,
             output_attentions=output_attentions,
             output_hidden_states=output_hidden_states,
+            start_position=start_position,
+            end_position=end_position
         )
         
         
         logits = self.para_classifier(outputs.pooler_output)
+        qa_logits = self.qa_output(outputs[0])
+        
+        start_logits, end_logits = start_logits.squeeze(-1).contiguous()
+        end_logits = end_logits.squeeze(-1).contiguous()
+        
         
         loss = None
         
@@ -157,6 +165,26 @@ class JointQAModel(BertPreTrainedModel):
                     loss += loss2
                     
             #add loss function
+            
+        
+            ## added code
+            if start_positions is not None and end_positions is not None:
+                # If we are on multi-GPU, split add a dimension
+                if len(start_positions.size()) > 1:
+                    start_positions = start_positions.squeeze(-1)
+                if len(end_positions.size()) > 1:
+                    end_positions = end_positions.squeeze(-1)
+                # sometimes the start/end positions are outside our model inputs, we ignore these terms
+                ignored_index = start_logits.size(1)
+                start_positions = start_positions.clamp(0, ignored_index)
+                end_positions = end_positions.clamp(0, ignored_index)
+
+                loss_fct = CrossEntropyLoss(ignore_index=ignored_index)
+                start_loss = loss_fct(start_logits, start_positions)
+                end_loss = loss_fct(end_logits, end_positions)
+                total_loss = (start_loss + end_loss) / 2
+            ##
+            
             
             else:
                 loss_fct = CrossEntropyLoss()
