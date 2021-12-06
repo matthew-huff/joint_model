65e77042efc531f430919490e94ab6cf0af144ef
Matthew Huff <mwhuff@pop-os.localdomain>
2021-12-05 17:53:45 -0700

    running into data loading issues

--- model_train/model.py
+++ model_train/model.py
@@ -146,8 +146,7 @@ class JointQAModel(BertPreTrainedModel):
         logits = self.para_classifier(outputs.pooler_output)
         qa_logits = self.qa_output(outputs[0])
         
-        start_logits, end_logits = start_logits.squeeze(-1).contiguous()
-        end_logits = end_logits.squeeze(-1).contiguous()
+       
         
         
         loss = None
@@ -167,23 +166,7 @@ class JointQAModel(BertPreTrainedModel):
             #add loss function
             
         
-            ## added code
-            if start_positions is not None and end_positions is not None:
-                # If we are on multi-GPU, split add a dimension
-                if len(start_positions.size()) > 1:
-                    start_positions = start_positions.squeeze(-1)
-                if len(end_positions.size()) > 1:
-                    end_positions = end_positions.squeeze(-1)
-                # sometimes the start/end positions are outside our model inputs, we ignore these terms
-                ignored_index = start_logits.size(1)
-                start_positions = start_positions.clamp(0, ignored_index)
-                end_positions = end_positions.clamp(0, ignored_index)
 
-                loss_fct = CrossEntropyLoss(ignore_index=ignored_index)
-                start_loss = loss_fct(start_logits, start_positions)
-                end_loss = loss_fct(end_logits, end_positions)
-                total_loss = (start_loss + end_loss) / 2
-            ##
             
             
             else:
